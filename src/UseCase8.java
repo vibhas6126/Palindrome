369f65639cdbecfa33e311013c0d56f7d10f150e
vibhas6126 <vibhasadusumilli@gmail.com>
2026-03-09 10:01:52 +0530

    Updated UC8 and added UC12: Strategy Pattern Palindrome Checker

--- src/UseCase8.java
+++ src/UseCase8.java
@@ -1,4 +1,88 @@
-package PACKAGE_NAME;
+// UC8: Linked List Based Palindrome Checker
 
-public class UseCase8 {
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class UseCase8{
+
+    // Function to check palindrome using linked list
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Find middle using fast and slow pointer
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        // Compare first half and reversed second half
+        Node first = head;
+        Node second = prev;
+
+        while (second != null) {
+            if (first.data != second.data) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+
+    // Convert string to linked list
+    public static Node createLinkedList(String word) {
+        Node head = null;
+        Node tail = null;
+
+        for (char c : word.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        String word = "level";
+
+        Node head = createLinkedList(word);
+
+        if (isPalindrome(head)) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+    }
 }
