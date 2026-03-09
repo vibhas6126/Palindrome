edca7870ef566f3e97347d24b9f0fc869ca54ac0
vibhas6126 <vibhasadusumilli@gmail.com>
2026-03-09 08:58:03 +0530

    Base application setup

---
+++ src/UseCase1.java
@@ -0,0 +1,13 @@
+
+public class UseCase1 {
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println(" Welcome to Palindrome Checker ");
+        System.out.println("==================================");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version : 1.0");
+        System.out.println("Application started successfully.");
+        System.out.println("Program will proceed to next use case...");
+
+    }
+}
