3903f055e4c6fc6aac953ab79522c24b4a5580e8
vibhas6126 <vibhasadusumilli@gmail.com>
2026-03-09 09:50:59 +0530

    UC11: Object Oriented Palindrome Checker

---
+++ src/UseCase11.java
@@ -0,0 +1,40 @@
+// UC11: Object-Oriented Palindrome Service
+
+class PalindromeChecker {
+
+    // Method to check palindrome
+    public boolean checkPalindrome(String word) {
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+public class UseCase11 {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        // Create object of PalindromeChecker
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
+
+        if (result) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+}
