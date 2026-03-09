5b59b2d912a8e045fc263d270f3a27a6bd6c224e
vibhas6126 <vibhasadusumilli@gmail.com>
2026-03-09 09:26:40 +0530

    UC7: Deque-based optimized palindrome checker

---
+++ src/UseCase7.java
@@ -0,0 +1,43 @@
+// UC7: Deque-Based Optimized Palindrome Checker
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class UseCase7 {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "level";
+
+        // Create a Deque
+        Deque<Character> deque = new ArrayDeque<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear characters
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+
+    }
+}
