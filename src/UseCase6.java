a185abd8970cb107f628389eddd8911941d7feba
vibhas6126 <vibhasadusumilli@gmail.com>
2026-03-09 09:23:49 +0530

    UC6: Queue and Stack based palindrome checker

---
+++ src/UseCase6.java
@@ -0,0 +1,44 @@
+// UC6: Queue + Stack Based Palindrome Check
+
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class UseCase6 {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "level";
+
+        // Create Stack and Queue
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Insert characters into both structures
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            stack.push(ch);     // LIFO
+            queue.add(ch);      // FIFO
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue and pop
+        while (!stack.isEmpty()) {
+
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+
+    }
+}
